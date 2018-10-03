e668cb17cdcbab058a11398b0344bbbbce2fb4b0
Daniele Viganò <daniele@vigano.me>
2018-10-03 15:51:36 +0200

    Trap SIGTERM during docker stop

--- start-xvfb-nginx.sh
+++ start-xvfb-nginx.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+cleanup() {
+    kill $XVFB_PID $QGIS_PID
+}
+
+trap cleanup SIGINT SIGTERM
+
 rm -f /tmp/.X99-lock
 /usr/bin/Xvfb :99 -ac -screen 0 1280x1024x16 +extension GLX +render -noreset >/dev/null &
 while ! pidof /usr/bin/Xvfb >/dev/null; do
     sleep 1
 done
-spawn-fcgi -u qgis -g qgis -d /tmp -p 9993 -- /usr/libexec/qgis/qgis_mapserv.fcgi
-nginx -g "daemon off;";
+XVFB_PID=$(pidof /usr/bin/Xvfb)
+spawn-fcgi -u qgis -g qgis -d /tmp -P /tmp/qgis.pid -p 9993 -- /usr/libexec/qgis/qgis_mapserv.fcgi
+QGIS_PID=$(pidof /usr/libexec/qgis/qgis_mapserv.fcgi)
+exec nginx -g "daemon off;";
