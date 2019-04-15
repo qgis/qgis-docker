cc58ef4cfdd793ac50583f7fbb3af44b62fa9ac2
Daniele Viganò <daniele@vigano.me>
2019-04-15 15:24:53 +0200

    Make QGIS the master process and set Nginx as optional

--- start-xvfb-nginx.sh
+++ start-xvfb-nginx.sh
@@ -24,18 +24,29 @@ cleanup() {
     # Timeout is managed directly by Docker, via it's '-t' flag:
     # if SIGTERM does not teminate the entrypoint, after the time
     # defined by '-t' (default 10 secs) the container is killed
-    kill $XVFB_PID $NGINX_PID
+    kill $XVFB_PID $QGIS_PID $NGINX_PID
+}
+
+waitfor() {
+    # Make startup syncronous
+    while ! pidof $1 >/dev/null; do
+        sleep 1
+    done
+    pidof $1
 }
 
 trap cleanup SIGINT SIGTERM
 
 rm -f /tmp/.X99-lock
 /usr/bin/Xvfb :99 -ac -screen 0 1280x1024x16 +extension GLX +render -noreset >/dev/null &
-while ! pidof /usr/bin/Xvfb >/dev/null; do
-    sleep 1
-done
-XVFB_PID=$(pidof /usr/bin/Xvfb)
-nginx
-NGINX_PID=$(pidof /usr/bin/nginx)
+XVFB_PID=$(waitfor /usr/bin/Xvfb)
+# Do not start NGINX if environment variable '$SKIP_NGINX' is set
+# this may be useful in production where an external reverse proxy is used
+if [ -z $SKIP_NGINX ]; then
+    nginx
+    NGINX_PID=$(waitfor /usr/sbin/nginx)
+fi
 # To avoid issues with GeoPackages when scaling out QGIS should not run as root
-exec spawn-fcgi -n -u ${QGIS_USER:-nginx} -g ${QGIS_USER:-nginx} -d /var/lib/qgis -P /run/qgis.pid -p 9993 -- /usr/libexec/qgis/qgis_mapserv.fcgi
+spawn-fcgi -n -u ${QGIS_USER:-nginx} -g ${QGIS_USER:-nginx} -d /var/lib/qgis -P /run/qgis.pid -p 9993 -- /usr/libexec/qgis/qgis_mapserv.fcgi &
+QGIS_PID=$(waitfor /usr/libexec/qgis/qgis_mapserv.fcgi)
+wait $QGIS_PID
