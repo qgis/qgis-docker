6998d2618617f6a771aa137808eaf62a5672c2d1
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-08-13 11:45:57 +0200

    Github workflow to automatically build Docker images (#42)

---
+++ scripts/get_docker_image_version.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# This scripts will output the QGIS versions on which LTR and stable rely on
+# Formatted as json: {"stable": "3.14.0", "ltr": "3.10.7"}
+
+import requests
+import json
+
+url = 'https://registry.hub.docker.com/v2/repositories/openquake/qgis-server/tags?page_size=10000'
+data = requests.get(url).content.decode('utf-8')
+tags = json.loads(data)['results']
+
+# get available tags
+availables_tags = {}
+for tag in tags:
+    if not tag['name'].endswith('-ubuntu'):
+        continue
+    if tag['name'].startswith('stable'):
+        stable = tag['images'][0]['digest']  # sha
+    elif tag['name'].startswith('ltr'):
+        ltr = tag['images'][0]['digest']  # sha
+    else:
+        tag_name = tag['name'].strip('-ubuntu')
+        availables_tags[tag_name] = tag['images'][0]['digest']
+
+# determine what is ltr and stable
+for tag, sha in availables_tags.items():
+    if sha == stable:
+        stable = tag
+    elif sha == ltr:
+        ltr = tag
+
+output = {'stable': stable, 'ltr': ltr}
+print(json.dumps(output))
---
+++ scripts/get_qgis_package_version.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# This scripts will output the last LTR and stable QGIS versions for Ubuntu
+# Formatted as json: {"stable": "3.14.0", "ltr": "3.10.7"}
+
+from apt_repo import APTRepository
+import argparse
+import re
+import json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dist', help='The Ubuntu distribution', default='bionic')
+    args = parser.parse_args()
+    dist = args.dist
+
+    data = {}
+    for ltr in (True, False):
+        url = 'http://qgis.org/ubuntu{}'.format('-ltr' if ltr else '')
+        components = ['main']
+        repo = APTRepository(url, dist, components)
+        package = repo.get_packages_by_name('qgis-server')[0]
+        assert package.package == 'qgis-server'
+        # https://regex101.com/r/lkuibv/1
+        p = re.compile('^1:(\d(?:\.\d+)+)(?:\+\d+{})$'.format(dist))
+        m = p.match(package.version)
+        data['ltr' if ltr else 'stable'] = m.group(1)
+
+    print(json.dumps(data))
