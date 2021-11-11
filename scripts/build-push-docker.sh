b3caab5acc5021d5169ee363216ba17ad1db1a90
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-11-11 10:42:51 +0100

    push to opengisch

--- scripts/build-push-docker.sh
+++ scripts/build-push-docker.sh
@@ -21,16 +21,16 @@ echo "MAJOR_QGIS_VERSION: ${MAJOR_QGIS_VERSION}"
 echo "UBUNTU_DIST: ${UBUNTU_DIST}"
 echo "QGIS_UBUNTU_PPA: ${QGIS_UBUNTU_PPA}"
 
-docker build --build-arg ubuntu_dist=${UBUNTU_DIST} --build-arg repo=${QGIS_UBUNTU_PPA} -t openquake/qgis-server:${RELEASE_TYPE} .
+docker build --build-arg ubuntu_dist=${UBUNTU_DIST} --build-arg repo=${QGIS_UBUNTU_PPA} -t opengisch/qgis-server:${RELEASE_TYPE} .
 
 for tag in '' '-ubuntu'; do
-    docker tag openquake/qgis-server:${RELEASE_TYPE} openquake/qgis-server:${RELEASE_TYPE}${tag}
-    docker tag openquake/qgis-server:${RELEASE_TYPE} openquake/qgis-server:${MAJOR_QGIS_VERSION}${tag}
-    docker tag openquake/qgis-server:${RELEASE_TYPE} openquake/qgis-server:${QGIS_VERSION}${tag}
+    docker tag opengisch/qgis-server:${RELEASE_TYPE} openquake/qgis-server:${RELEASE_TYPE}${tag}
+    docker tag opengisch/qgis-server:${RELEASE_TYPE} openquake/qgis-server:${MAJOR_QGIS_VERSION}${tag}
+    docker tag opengisch/qgis-server:${RELEASE_TYPE} openquake/qgis-server:${QGIS_VERSION}${tag}
 done
 
 for tag in '' '-ubuntu'; do
-    docker push openquake/qgis-server:${RELEASE_TYPE}${tag}
-    docker push openquake/qgis-server:${MAJOR_QGIS_VERSION}${tag}
-    docker push openquake/qgis-server:${QGIS_VERSION}${tag}
+    docker push opengisch/qgis-server:${RELEASE_TYPE}${tag}
+    docker push opengisch/qgis-server:${MAJOR_QGIS_VERSION}${tag}
+    docker push opengisch/qgis-server:${QGIS_VERSION}${tag}
 done
